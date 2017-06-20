430b95b9834abbbe90fecddcac9e920869072378
cleberfonseca-luizalabs <cleber.avance@luizalabs.com>
2017-06-20 01:50:13 -0300

    A nova rota foi criada para listar os game

--- routes/routes.js
+++ routes/routes.js
@@ -1,13 +1,16 @@
-module.exports = function(app) {
+module.exports = function (app) {
 
     var config = require('config');
+    var gameReportController = app.modules.controllers.gameReportController;
 
-    app.get('/api/version', function(req, res) {
+    app.get('/api/version', function (req, res) {
         res.status(200).send({
             app: "Quake Report API",
             version: 'v1.0.0',
             enviroment: process.env.NODE_ENV
         });
     });
+
+    app.get('/api/' + config.apiVersion + '/games', gameReportController.getGamesReport);
 
 };
