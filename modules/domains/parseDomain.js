9795aea01f2d3ccbb4647faddacd597bed81a651
cleberfonseca-luizalabs <cleber.avance@luizalabs.com>
2017-06-19 21:18:33 -0300

    Adicionado os testes para os métodos de verificação para as linhas do arquivo. Tipos: inicio de jogo, evento de jogador, morte e fim de jogo.

--- modules/domains/parseDomain.js
+++ modules/domains/parseDomain.js
@@ -8,15 +8,42 @@ module.exports = function (app) {
 
     var parseDomain = {
 
-        readFile: function (path, callback) {
-            fs.exists(path, function(exists) {
-                console.log(exists);
+        _readFile: function (path, callback) {
+            fs.exists(path, function (exists) {
                 if (exists) {
-                    callback(null);
+                    fs.readFile(path, 'utf8', function (err, data) {
+                        if (err) callback(err);
+                        var lines = data.split(/\r?\n/);
+                        callback(err, lines);
+                    });
                 } else {
                     callback("Arquivo não localizado");
                 }
             });
+        },
+
+        _initGame: function (line, callback) {
+            var regex = "\\s*\\d{1,2}:\\d{2}\\s*(InitGame)\\w*";
+            var aReturn = line.match(regex);
+            callback((Array.isArray(aReturn) === true));
+        },
+
+        _endGame: function (line, callback) {
+            var regex = "\\s*\\d{1,2}:\\d{2}\\s*(ShutdownGame)\\w*";
+            var aReturn = line.match(regex);
+            callback((Array.isArray(aReturn) === true));
+        },
+
+        _killEvent: function (line, callback) {
+            var regex = "\\s*\\d{1,2}:\\d{2}\\s*(Kill)\\w*";
+            var aReturn = line.match(regex);
+            callback((Array.isArray(aReturn) === true));
+        },
+
+        _clientEvent: function (line, callback) {
+            var regex = "\\s*\\d{1,2}:\\d{2}\\s*(ClientUserinfoChanged)\\w*";
+            var aReturn = line.match(regex);
+            callback((Array.isArray(aReturn) === true));
         }
     };
 
--- test/modules/domains/parseDomainTest.js
+++ test/modules/domains/parseDomainTest.js
@@ -1,10 +1,8 @@
-"use strict";
-
 /***
  * @author Cleber Fonseca
  * @date 19/06/17.
  */
-describe("Testa o parseDomain", function() {
+describe("Testa o parseDomain", function () {
 
     var app;
     var should;
@@ -13,14 +11,16 @@ describe("Testa o parseDomain", function() {
     var sinon;
     this.timeout(10000);
 
-    before(function(done) {
+    var initGameLine = '  0:00 InitGame: \sv_floodProtect\1\sv_maxPing\0\sv_minPing\0\sv_maxRate\10000\sv_minRate\0\sv_hostname\Code Miner Server\g_gametype\0\sv_privateClients\2\sv_maxclients\16\sv_allowDownload\0\dmflags\0\fraglimit\20\timelimit\15\g_maxGameClients\0\capturelimit\8\version\ioq3 1.36 linux-x86_64 Apr 12 2009\protocol\68\mapname\q3dm17\gamename\baseq3\g_needpass\0';
+    var endGameLine = '  1:47 ShutdownGame:';
+    var killEvent = '  1:11 Kill: 1022 4 19: <world> killed Isgalamido by MOD_FALLING';
+    var clientEvent = '  1:02 ClientUserinfoChanged: 7 n\Mal\t\2\model\james\hmodel\*james\g_redteam\\g_blueteam\\c1\4\c2\5\hc\100\w\0\l\0\tt\0\tl\0';
+
+    before(function (done) {
 
         process.env.NODE_ENV = 'test';
-
         app = require('../../../app');
-
         parseDomain = app.modules.domains.parseDomain;
-
         should = require('should');
         config = require('config');
         sinon = require('sinon');
@@ -28,16 +28,129 @@ describe("Testa o parseDomain", function() {
         done();
     });
 
-    it("Deveria falhar ao tentar ler um arquivo que não existe", function(done) {
-        parseDomain.readFile("invalido", function(err) {
+    it("Deveria falhar ao tentar ler um arquivo que não existe", function (done) {
+        parseDomain._readFile("invalido", function (err) {
             (err === "Arquivo não localizado").should.be.true();
             done();
         });
     });
 
-    it("Deveria executar a leitura do arquivo e retornar o array com as linhas", function(done) {
-        parseDomain.readFile("file_to_parse/games.log", function(err) {
+    it("Deveria executar a leitura do arquivo e retornar o array com as linhas", function (done) {
+        parseDomain._readFile("file_to_parse/games.log", function (err, lines) {
             (err === null).should.be.true();
+            (Array.isArray(lines) === true).should.be.true();
+            done();
+        });
+    });
+
+    it("Deveria indicar que iniciou o jogo", function (done) {
+        parseDomain._initGame(initGameLine, function (match) {
+            (match === true).should.be.true();
+            done();
+        });
+    });
+
+    it("Deveria indicar que não iniciou o jogo, ao informar um evento de kill", function (done) {
+        parseDomain._initGame(killEvent, function (match) {
+            (match === false).should.be.true();
+            done();
+        });
+    });
+
+    it("Deveria indicar que não iniciou o jogo, ao informar um evento do jogador", function (done) {
+        parseDomain._initGame(clientEvent, function (match) {
+            (match === false).should.be.true();
+            done();
+        });
+    });
+
+    it("Deveria indicar que não iniciou o jogo, ao informar um evento de fim do jogo", function (done) {
+        parseDomain._initGame(endGameLine, function (match) {
+            (match === false).should.be.true();
+            done();
+        });
+    });
+
+    it("Deveria indicar que finalizou o jogo", function (done) {
+        parseDomain._endGame(endGameLine, function (match) {
+            (match === true).should.be.true();
+            done();
+        });
+    });
+
+    it("Deveria indicar que não finalizou o jogo, ao informar um evento de kill", function (done) {
+        parseDomain._endGame(killEvent, function (match) {
+            (match === false).should.be.true();
+            done();
+        });
+    });
+
+    it("Deveria indicar que não finalizou o jogo, ao informar um evento do jogador", function (done) {
+        parseDomain._endGame(clientEvent, function (match) {
+            (match === false).should.be.true();
+            done();
+        });
+    });
+
+    it("Deveria indicar que não finalizou o jogo, ao informar um evento de início de jogo", function (done) {
+        parseDomain._endGame(initGameLine, function (match) {
+            (match === false).should.be.true();
+            done();
+        });
+    });
+
+    it("Deveria indicar que houve uma morte no jogo", function (done) {
+        parseDomain._killEvent(killEvent, function (match) {
+            (match === true).should.be.true();
+            done();
+        });
+    });
+    
+    it("Deveria indicar que não houve uma morte no jogo, ao informar um evento de fim de jogo", function (done) {
+        parseDomain._killEvent(endGameLine, function (match) {
+            (match === false).should.be.true();
+            done();
+        });
+    });
+
+    it("Deveria indicar que não houve uma morte no jogo, ao informar um evento do jogador", function (done) {
+        parseDomain._killEvent(clientEvent, function (match) {
+            (match === false).should.be.true();
+            done();
+        });
+    });
+
+    it("Deveria indicar que não houve uma morte no jogo, ao informar um evento de início de jogo", function (done) {
+        parseDomain._killEvent(initGameLine, function (match) {
+            (match === false).should.be.true();
+            done();
+        });
+    });
+
+    it("Deveria indicar que houve um evento de jogador", function (done) {
+        parseDomain._clientEvent(clientEvent, function (match) {
+            (match === true).should.be.true();
+            done();
+        });
+    });
+    
+    it("Deveria indicar que não houve um evento de jogador, ao informar um evento de fim de jogo", function (done) {
+        parseDomain._clientEvent(endGameLine, function (match) {
+            (match === false).should.be.true();
+            done();
+        });
+    });
+
+    it("Deveria indicar que não houve um evento de jogador, ao informar um evento de kill", function (done) {
+        parseDomain._clientEvent(killEvent, function (match) {
+            (match === false).should.be.true();
+            done();
+        });
+    });
+
+    it("Deveria indicar que não houve um evento de jogador, ao informar um evento de início de jogo", function (done) {
+        parseDomain._clientEvent(initGameLine, function (match) {
+            (match === false).should.be.true();
             done();
         });
     });
